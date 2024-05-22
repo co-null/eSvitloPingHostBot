c5b86dc76809af94310517e10cbd930cf77816f5
Kyrylo Onyshchenko <kyrylo.onyshchenko@accuristech.com>
2024-05-22 03:09:30 +0300

    Added listener

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # Replace 'YOUR_BOT_TOKEN' with the token you got from BotFather
-BOT_TOKEN="6740356765:AAEyU9C-bJKRn6onTbTvFxGeYWyF1V42GhU"
+BOT_TOKEN=""
 SETTINGS_FILE = 'user_settings.json'
 STATES_FILE = 'user_states.json'
 isPostOK="F"
 msg_greeting="Вітаю! Налаштуйте бот для перевірки доступності IP адреси кожну хвилину (для старту моніторингу адреса має бути доступна), або ввімкніть режим прослуховування. Є можливість публікації повідомлень у канал (для цього попередньо додайте бот в канал)"
+msg_error="Виникла помилка, перезапустіть бота"
 msg_mainmnu="Головне меню"
 msg_setip="Будь ласка, введіть IP адресу для моніторингу:"
 msg_setlabel="Будь ласка введіть назву адреси для моніторингу:"
