ad87e7f6e9a2d109910d4fea9fe6c8dc1b700c10
Kyrylo Onyshchenko <kyrylo.onyshchenko@accuristech.com>
2024-06-06 20:48:42 +0300

    News broadcasting

--- teleclient.py
+++ teleclient.py
@@ -1,17 +1,39 @@
 from telethon import TelegramClient, events, sync, utils
 import tele_secrets
 import config as cfg, user_settings as us
+from datetime import datetime 
+import pytz
 
+use_tz = pytz.timezone(cfg.TZ)
+
+monitor = TelegramClient('monitor_session', tele_secrets.api_id, tele_secrets.api_hash)
 client = TelegramClient('bot_session', tele_secrets.api_id, tele_secrets.api_hash)
 
-@client.on(events.NewMessage(chats = [cfg.DTEK_CHANNEL_ID]))
+monitor.start()
+
+@monitor.on(events.NewMessage(chats = [cfg.DTEK_CHANNEL_ID, '@eSvitloPingHostSource']))
 async def newMessageListener(event):
+    dt = datetime.now(use_tz)
     msg = event.message
-    #print("Event Occured")
+    print(f"{dt} Monitor Event Occured")
     if 'відключен' in event.raw_text.lower():
-        for user_id in us.user_settings.keys():
-            if us.user_settings[user_id]['to_channel'] and us.user_settings[user_id]['channel_id']:
-                await client.forward_messages(entity=us.user_settings[user_id]['channel_id'], messages=msg)
+        await monitor.forward_messages(entity='@eSvitloPingHostBotBuffer', messages=msg)
+
+##client.start()
+
+@client.on(events.NewMessage(chats = ['@eSvitloPingHostBotBuffer']))
+async def newMessageSender(event):
+    dt = datetime.now(use_tz)
+    msg = event.message
+    print(f"{dt} Client Event Occured")
+    for user_id in us.user_settings.keys():
+        if us.user_settings[user_id]['to_channel'] and us.user_settings[user_id]['channel_id']:
+            print(f"{dt} Send to {us.user_settings[user_id]['channel_id']}")
+            await client.forward_messages(entity=us.user_settings[user_id]['channel_id'], messages=msg)
+    await client.delete_messages(entity='t.me/eSvitloPingHostBotBuffer', message_ids=[msg.id])
 
 with client:
     client.run_until_disconnected()
+
+with monitor:
+    monitor.run_until_disconnected()
